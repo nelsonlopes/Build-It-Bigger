d20a5247913c788f18c28c6ba02a1f3fab8a2967
Nelson Lopes <nelopes@expressglass.pt>
2019-11-08 12:05:23 +0000

    feat: Add JokesDisplayer Activity to the Android library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.nelsonlopes.javajokes.Joker;
+import com.nelsonlopes.jokesdisplayer.
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
         Joker joker = new Joker();
         String joke = joker.getJoke();
+
+        Intent intent = new Intent(this, )
         Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ jokesdisplayer/src/main/java/com/nelsonlopes/jokesdisplayer/JokesDisplayer.java
@@ -0,0 +1,13 @@
+package com.nelsonlopes.jokesdisplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokesDisplayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes_displayer);
+    }
+}
