ccddaa8e29e3a2fc8d8d42f1a44a7dc26e2b4853
Nelson Lopes <nelopes@expressglass.pt>
2019-11-08 11:14:22 +0000

    feat: Step 1: Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import com.nelsonlopes.javajokes.Joker;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
     }
-
-
 }
