b708c8451a4f42ff0b4896476b7ee098bc17c8d2
Nelson Lopes <nelopes@expressglass.pt>
2019-11-12 21:21:27 +0000

    fix: Ads library dependencies is now used on the free flavor only

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -5,11 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.udacity.gradle.builditbigger.R;
 
 /**
  * A placeholder fragment containing a simple view.
